a909a185c2184591c6875b9389c2b29b2afcfb24
John Bracken <strawhorse@gmail.com>
2026-01-27 14:38:02 +0000

    Converted the Product Java class into a JPA entity

--- src/main/java/com/jbr/ecommercespring/controller/PageController.java
+++ src/main/java/com/jbr/ecommercespring/controller/PageController.java
@@ -1,7 +1,11 @@
 package com.jbr.ecommercespring.controller;
 
+import com.jbr.ecommercespring.model.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class PageController {
@@ -11,9 +15,26 @@ public class PageController {
         return "home";
     }
 
+//    @GetMapping("/products")
+//    public String products() {
+//        return "products";
+//    }
+
+    // new product controller
     @GetMapping("/products")
-    public String products() {
+    public String products(Model model) {
+
+        List<Product> products = List.of(
+                new Product(1L, "Laptop", "Powerful and yet portable", 999.99, "https://via.placeholder.com/300"),
+                new Product(1L, "Headphones", "Insert text here", 99.99, "https://via.placeholder.com/300"),
+                new Product(1L, "Smartphone", "Insert text here", 299.99, "https://via.placeholder.com/300")
+        );
+
+        model.addAttribute("products", products);
         return "products";
+
     }
+
+
 
 }
---
+++ src/main/java/com/jbr/ecommercespring/model/Product.java
@@ -0,0 +1,58 @@
+package com.jbr.ecommercespring.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 2000)
+    private String description;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private double price;
+
+
+    private String imageUrl;
+
+    public Product(Long id, String name, String description, double price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Product() {
+        // JPA needs a no-args constructor
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
